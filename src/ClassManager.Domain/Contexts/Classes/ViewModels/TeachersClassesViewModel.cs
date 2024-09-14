6c3d6a70b29d0554046fffccbd35933383694801
Mateus Lopes <devmateustools@gmail.com>
2024-09-14 20:04:33 -0300

    feat: teachers classes view model

---
+++ src/ClassManager.Domain/Contexts/Classes/ViewModels/TeachersClassesViewModel.cs
@@ -0,0 +1,12 @@
+namespace ClassManager.Domain.Contexts.Classes.ViewModels
+{
+  public class TeachersClassesViewModel
+  {
+    public Guid Id { get; private set; }
+    public Guid UserId { get; private set; }
+    public Guid ClassId { get; private set; }
+    public ClassViewModel Class { get; private set; } = null!;
+    public UserViewModel User { get; private set; } = null!;
+    public DateTime CreatedAt { get; private set; }
+  }
+}
