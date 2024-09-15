216a570505247f1051260af40bc6f0b92b8d6620
Mateus Lopes <devmateustools@gmail.com>
2024-09-15 00:11:47 -0300

    feat: get tenant mapped

--- src/ClassManager.Domain/Contexts/Tenants/Handlers/GetTenantHandler.cs
+++ src/ClassManager.Domain/Contexts/Tenants/Handlers/GetTenantHandler.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using ClassManager.Domain.Contexts.tenants.ViewModels;
 using ClassManager.Domain.Contexts.Tenants.Repositories.Contracts;
 using ClassManager.Domain.Shared.Commands;
 using ClassManager.Shared.Commands;
@@ -7,16 +9,19 @@ namespace ClassManager.Domain.Contexts.Tenants.Handlers;
 public class GetTenantHandler
 {
   private readonly ITenantRepository _repository;
+  private readonly IMapper _mapper;
   public GetTenantHandler(
-    ITenantRepository tenantRepository
+    ITenantRepository tenantRepository,
+    IMapper mapper
     )
   {
     _repository = tenantRepository;
+    _mapper = mapper;
   }
   public async Task<ICommandResult> Handle(Guid id)
   {
-    var tenant = await _repository.GetByIdAsync(id, new CancellationToken());
+    var tenant = _mapper.Map<TenantViewModel>(await _repository.GetByIdAsync(id, new CancellationToken()));
 
-    return new CommandResult(true, "TENANT", tenant, null, 201);
+    return new CommandResult(true, "TENANT_GOTTEN", tenant, null, 201);
   }
 }
