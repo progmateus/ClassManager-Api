379863e21d0322615698e9b4556e307079a99c95
Mateus Lopes <devmateustools@gmail.com>
2024-09-15 00:06:31 -0300

    feat: list users roles mapped

--- src/ClassManager.Domain/Contexts/Roles/Handlers/ListUsersByRoleHandler.cs
+++ src/ClassManager.Domain/Contexts/Roles/Handlers/ListUsersByRoleHandler.cs
@@ -1,5 +1,7 @@
+using AutoMapper;
 using ClassManager.Domain.Contexts.Accounts.Repositories.Contracts;
 using ClassManager.Domain.Contexts.Roles.Repositories.Contracts;
+using ClassManager.Domain.Contexts.Roles.ViewModels;
 using ClassManager.Domain.Shared.Commands;
 using ClassManager.Shared.Commands;
 
@@ -8,16 +10,19 @@ namespace ClassManager.Domain.Contexts.Accounts.Handlers;
 public class ListUsersRolesHandler
 {
   private readonly IUsersRolesRepository _usersRolesRepository;
+  private readonly IMapper _mapper;
   public ListUsersRolesHandler(
-    IUsersRolesRepository userRepository
+    IUsersRolesRepository userRepository,
+     IMapper mapper
     )
   {
     _usersRolesRepository = userRepository;
+    _mapper = mapper;
   }
   public async Task<ICommandResult> Handle(Guid tenantId, List<string> rolesNames, List<Guid> usersIds)
   {
-    var users = await _usersRolesRepository.ListByRoleAsync(tenantId, rolesNames, usersIds);
+    var usersRoles = _mapper.Map<UsersRolesViewModel>(await _usersRolesRepository.ListByRoleAsync(tenantId, rolesNames, usersIds));
 
-    return new CommandResult(true, "USERS_ROLES_LISTED", users, null, 200);
+    return new CommandResult(true, "USERS_ROLES_LISTED", usersRoles, null, 200);
   }
 }
