ccef36d3e84b92dde206e19f7ed719990bf149f1
Mateus Lopes <devmateustools@gmail.com>
2024-09-15 00:13:00 -0300

    feat: list tenants mappped

--- src/ClassManager.Domain/Contexts/Tenants/Handlers/ListTenantHandler.cs
+++ src/ClassManager.Domain/Contexts/Tenants/Handlers/ListTenantHandler.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using ClassManager.Domain.Contexts.tenants.ViewModels;
 using ClassManager.Domain.Contexts.Tenants.Repositories.Contracts;
 using ClassManager.Domain.Shared.Commands;
 using ClassManager.Shared.Commands;
@@ -7,15 +9,18 @@ namespace ClassManager.Domain.Contexts.Tenants.Handlers;
 public class ListTenantsHandler
 {
   private readonly ITenantRepository _repository;
+  private readonly IMapper _mapper;
   public ListTenantsHandler(
-    ITenantRepository tenantRepository
+    ITenantRepository tenantRepository,
+    IMapper mapper
     )
   {
     _repository = tenantRepository;
+    _mapper = mapper;
   }
   public async Task<ICommandResult> Handle(string search = "")
   {
-    var tenants = await _repository.SearchAsync(search);
+    var tenants = _mapper.Map<List<TenantViewModel>>(await _repository.SearchAsync(search));
 
     return new CommandResult(true, "TENANTS_LISTED", tenants, null, 201);
   }
