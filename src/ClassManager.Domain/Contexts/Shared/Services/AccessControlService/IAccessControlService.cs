b2a118dc2f067f170b1360c8cf0b089de6769e6d
Mateus Lopes <devmateustools@gmail.com>
2024-09-23 06:08:01 -0300

    inteface i access control service

---
+++ src/ClassManager.Domain/Contexts/Shared/Services/AccessControlService/IAccessControlService.cs
@@ -0,0 +1,10 @@
+
+namespace ClassManager.Domain.Shared.Services.AccessControlService;
+
+public interface IAccessControlService
+{
+  Task<bool> IsTenantActive(Guid tenantId);
+  Task<bool> IsTenantSubscriptionActive(Guid tenantId);
+  Task<bool> HasUserPermission(Guid userId, Guid tenantId, string roleName);
+  Task<bool> IsUserActiveSubscription(Guid userId, Guid tenantId);
+}
