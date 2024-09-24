ddb9d7345151b4261057bfaf3902742de3e2d709
Mateus Lopes <devmateustools@gmail.com>
2024-09-24 01:41:10 -0300

    feat: delete tenant plan with control service handler

--- src/ClassManager.Domain/Contexts/Tenants/Handlers/DeleteTenantPlanHandler.cs
+++ src/ClassManager.Domain/Contexts/Tenants/Handlers/DeleteTenantPlanHandler.cs
@@ -1,26 +1,46 @@
 using ClassManager.Domain.Contexts.Tenants.Repositories.Contracts;
 using ClassManager.Domain.Shared.Commands;
+using ClassManager.Domain.Shared.Services.AccessControlService;
 using ClassManager.Shared.Commands;
+using ClassManager.Shared.Handlers;
 
 namespace ClassManager.Domain.Contexts.Tenants.Handlers;
 
-public class DeleteTenantPlanHandler
+public class DeleteTenantPlanHandler : ITenantDeleteActionHandler
 {
-  private readonly ITenantPlanRepository _repository;
+  private readonly ITenantPlanRepository _tenantPlansRepository;
+  private readonly IAccessControlService _accessControlService;
+
   public DeleteTenantPlanHandler(
-    ITenantPlanRepository tenantPlanRepository
+    ITenantPlanRepository tenantPlanRepository,
+    IAccessControlService accessControlService
+
     )
   {
-    _repository = tenantPlanRepository;
+    _tenantPlansRepository = tenantPlanRepository;
+    _accessControlService = accessControlService;
+
   }
 
-  public async Task<ICommandResult> Handle(Guid tenantId, Guid id)
+  public async Task<ICommandResult> Handle(Guid loggedUserId, Guid tenantId, Guid entityId)
   {
-    if (await _repository.GetByIdAndTenantId(tenantId, id, new CancellationToken()) == null)
+
+    if (!await _accessControlService.IsTenantSubscriptionActiveAsync(tenantId))
+    {
+      return new CommandResult(false, "ERR_TENANT_INACTIVE", null, null);
+    }
+
+    if (await _accessControlService.HasUserRoleAsync(loggedUserId, tenantId, "admin"))
+    {
+      return new CommandResult(false, "ERR_ADMIN_ROLE_NOT_FOUND", null, null, 403);
+    }
+
+    if (await _tenantPlansRepository.GetByIdAndTenantId(tenantId, entityId, new CancellationToken()) == null)
     {
       return new CommandResult(false, "ERR_PLAN_NOT_FOUND", null, null, 404);
     }
-    await _repository.DeleteAsync(id, default);
+
+    await _tenantPlansRepository.DeleteAsync(entityId, default);
 
     return new CommandResult(true, "PLAN_DELETED", null, null, 204);
   }
