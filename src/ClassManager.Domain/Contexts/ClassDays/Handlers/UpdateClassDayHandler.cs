f5c6d757de5dd4acc58a4a2d95e0bcc8929c686a
Mateus Lopes <devmateustools@gmail.com>
2024-09-24 04:07:39 -0300

    feat: git add .

--- src/ClassManager.Domain/Contexts/ClassDays/Handlers/UpdateClassDayHandler.cs
+++ src/ClassManager.Domain/Contexts/ClassDays/Handlers/UpdateClassDayHandler.cs
@@ -2,6 +2,7 @@ using ClassManager.Domain.Contexts.ClassDays.Repositories.Contracts;
 using ClassManager.Domain.Contexts.Classes.Commands;
 using ClassManager.Domain.Contexts.Classes.Repositories.Contracts;
 using ClassManager.Domain.Shared.Commands;
+using ClassManager.Domain.Shared.Services.AccessControlService;
 using ClassManager.Shared.Commands;
 using ClassManager.Shared.Handlers;
 using Flunt.Notifications;
@@ -10,23 +11,39 @@ namespace ClassManager.Domain.Contexts.ClassDays.Handlers;
 
 public class UpdateClassDayHandler :
   Notifiable,
-  IActionHandler<UpdateClassDayCommand>
+  ITenantActionHandler<UpdateClassDayCommand>
 {
   private readonly IClassDayRepository _classDayRepository;
+  private readonly IAccessControlService _accessControlService;
+
 
   public UpdateClassDayHandler(
-    IClassDayRepository classDayRepository
+    IClassDayRepository classDayRepository,
+    IAccessControlService accessControlService
+
     )
   {
     _classDayRepository = classDayRepository;
+    _accessControlService = accessControlService;
+
   }
-  public async Task<ICommandResult> Handle(Guid classDayId, UpdateClassDayCommand command)
+  public async Task<ICommandResult> Handle(Guid loggedUserId, Guid tenantId, Guid classDayId, UpdateClassDayCommand command)
   {
     command.Validate();
     if (command.Invalid)
     {
       AddNotifications(command);
       return new CommandResult(false, "ERR_CLASS_DAY_NOT_UPDATED", null, command.Notifications);
+    }
+
+    if (!await _accessControlService.IsTenantSubscriptionActiveAsync(tenantId))
+    {
+      return new CommandResult(false, "ERR_TENANT_INACTIVE", null, null);
+    }
+
+    if (!await _accessControlService.HasUserRoleAsync(loggedUserId, tenantId, "admin"))
+    {
+      return new CommandResult(false, "ERR_ADMIN_ROLE_NOT_FOUND", null, null, 403);
     }
 
     var classDay = await _classDayRepository.GetByIdAsync(classDayId, new CancellationToken());
