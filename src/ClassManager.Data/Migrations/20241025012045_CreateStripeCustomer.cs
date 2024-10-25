f6ba6baf7e9afa3cb93a8b7f9bc0ed2a08da2f85
Mateus Lopes <devmateustools@gmail.com>
2024-10-24 22:21:32 -0300

    feat: create stripe customer migration

---
+++ src/ClassManager.Data/Migrations/20241025012045_CreateStripeCustomer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClassManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateStripeCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StripeCustomer",
+                columns: table => new
+                {
+                    StripeCustomerId = table.Column<string>(type: "varchar(100)", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TenantId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StripeCustomer", x => x.StripeCustomerId);
+                    table.ForeignKey(
+                        name: "FK_StripeCustomer_Tenants_TenantId",
+                        column: x => x.TenantId,
+                        principalTable: "Tenants",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_StripeCustomer_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StripeCustomer_TenantId",
+                table: "StripeCustomer",
+                column: "TenantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StripeCustomer_UserId",
+                table: "StripeCustomer",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StripeCustomer");
+        }
+    }
+}
