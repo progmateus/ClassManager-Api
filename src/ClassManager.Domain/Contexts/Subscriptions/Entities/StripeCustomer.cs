f2c2561f75c3a15871e72afdd139e4fce0713e2c
Mateus Lopes <devmateustools@gmail.com>
2024-10-27 20:20:46 -0300

    feat: adding type in stripe customer entity

--- src/ClassManager.Domain/Contexts/Subscriptions/Entities/StripeCustomer.cs
+++ src/ClassManager.Domain/Contexts/Subscriptions/Entities/StripeCustomer.cs
@@ -1,4 +1,5 @@
 using ClassManager.Domain.Contexts.Accounts.Entities;
+using ClassManager.Domain.Contexts.Shared.Enums;
 using ClassManager.Domain.Contexts.Tenants.Entities;
 using ClassManager.Domain.Shared.Entities;
 
@@ -7,15 +8,17 @@ public class StripeCustomer : TenantEntity
 {
   protected StripeCustomer() { }
 
-  public StripeCustomer(Guid userId, Guid tenantId, string stripeCustomerId)
+  public StripeCustomer(Guid userId, Guid tenantId, string stripeCustomerId, EStripeCustomerType type)
   {
     UserId = userId;
     TenantId = tenantId;
     StripeCustomerId = stripeCustomerId;
+    Type = type;
   }
 
   public Guid UserId { get; private set; }
   public string StripeCustomerId { get; private set; }
+  public EStripeCustomerType Type { get; private set; } = EStripeCustomerType.USER;
   public User? User { get; private set; }
   public Tenant? Tenant { get; private set; }
   public DateTime CreatedAt { get; private set; }
