bea69e5e7c99c44ec2f8cde6fd1f4c828a8815ac
Mateus Lopes <devmateustools@gmail.com>
2024-11-14 18:29:13 -0300

    feat: list by tenant id with pagination method

--- src/ClassManager.Data/Contexts/Shared/Repositories/TRepository.cs
+++ src/ClassManager.Data/Contexts/Shared/Repositories/TRepository.cs
@@ -93,4 +93,9 @@ public abstract class TRepository<TEntity> : ITRepository<TEntity> where TEntity
 
     return await includes.Aggregate(query, (current, includeProperty) => current.Include(includeProperty)).FirstOrDefaultAsync();
   }
+
+  public async Task<List<TEntity>> ListByTenantIdWithPagination(Guid tenantId, string search = "", int skip = 0, int limit = 30, CancellationToken cancellationToken = default)
+  {
+    return await DbSet.Where(x => x.TenantId == tenantId).Skip(skip).Take(limit).ToListAsync(cancellationToken);
+  }
 }
--- src/ClassManager.Domain/Contexts/Shared/Repositories/ITRepository.cs
+++ src/ClassManager.Domain/Contexts/Shared/Repositories/ITRepository.cs
@@ -5,6 +5,7 @@ public interface ITRepository<TEntity> where TEntity : TenantEntity
 {
   Task<TEntity?> FindByIdAndTenantIdAsync(Guid id, Guid tenantId, CancellationToken cancellationToken);
   Task<List<TEntity>> ListByTenantId(Guid tenantId, string search = "", int skip = 0, int limit = 30, CancellationToken cancellationToken = default);
+  Task<List<TEntity>> ListByTenantIdWithPagination(Guid tenantId, string search = "", int skip = 0, int limit = 30, CancellationToken cancellationToken = default);
   Task CreateAsync(TEntity entity, CancellationToken cancellationToken);
   Task UpdateAsync(TEntity entity, CancellationToken cancellationToken);
   Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includes);
