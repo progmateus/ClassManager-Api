5ae228730492e3a5946112e9fef5187c515916b4
Mateus Lopes <devmateustools@gmail.com>
2024-11-14 18:51:04 -0300

    feat: list repository method with pagination

--- src/ClassManager.Domain/Contexts/Plans/Handlers/ListPlansHandler.cs
+++ src/ClassManager.Domain/Contexts/Plans/Handlers/ListPlansHandler.cs
@@ -1,10 +1,11 @@
 using ClassManager.Domain.Contexts.Plans.Repositories;
 using ClassManager.Domain.Shared.Commands;
 using ClassManager.Shared.Commands;
+using ClassManager.Shared.Handlers;
 
 namespace ClassManager.Domain.Contexts.Plans.Handlers;
 
-public class ListPlansHandler
+public class ListPlansHandler : IPaginationHandler<PaginationCommand>
 {
   private readonly IPlanRepository _repository;
 
@@ -15,10 +16,10 @@ public class ListPlansHandler
     _repository = planRepository;
   }
 
-  public async Task<ICommandResult> Handle()
+  public async Task<ICommandResult> Handle(Guid loggedUserId, PaginationCommand command)
   {
 
-    var plans = await _repository.GetAllAsync(new CancellationToken());
+    var plans = await _repository.List(new CancellationToken());
 
     return new CommandResult(true, "PLANS_LISTED", plans, null, 204);
   }
