49bf5f58a88ab0997b8e65745867deaea7e616b6
Mateus Lopes <devmateustools@gmail.com>
2024-12-05 16:28:40 -0300

    feat: address contract repository

---
+++ src/ClassManager.Domain/Contexts/Address/Contracts/IAddressRepository.cs
@@ -0,0 +1,6 @@
+
+
+using ClassManager.Domain.Contexts.Addresses.Entites;
+
+namespace ClassManager.Domain.Contexts.Addresses.Repositories.Contracts;
+public interface IAddressRepository : IRepository<Address> { }
--- src/ClassManager.Domain/Contexts/Address/Entities/Address.cs
+++ src/ClassManager.Domain/Contexts/Address/Entities/Address.cs
@@ -2,27 +2,25 @@ using ClassManager.Domain.Contexts.Accounts.Entities;
 using ClassManager.Domain.Contexts.Tenants.Entities;
 using ClassManager.Domain.Shared.Entities;
 
-namespace ClassManager.Domain.Contexts.Address.Entites
+namespace ClassManager.Domain.Contexts.Addresses.Entites;
+public class Address : Entity
 {
-  public class Address : Entity
+  protected Address() { }
+  public Address(string street, string city, string state)
   {
-    protected Address() { }
-    public Address(string street, string city, string state)
-    {
-      Street = street;
-      City = city;
-      State = state;
-    }
-    public Guid? UserId { get; private set; }
-    public Guid? TenantId { get; private set; }
-    public string Street { get; private set; }
-    public string? Number { get; private set; }
-    public string? Neighborhood { get; private set; }
-    public string City { get; private set; }
-    public string State { get; private set; }
-    public string Country { get; private set; } = "BR";
-    public string? ZipCode { get; private set; }
-    public User? User { get; private set; }
-    public Tenant? Tenant { get; private set; }
+    Street = street;
+    City = city;
+    State = state;
   }
+  public Guid? UserId { get; private set; }
+  public Guid? TenantId { get; private set; }
+  public string Street { get; private set; }
+  public string? Number { get; private set; }
+  public string? Neighborhood { get; private set; }
+  public string City { get; private set; }
+  public string State { get; private set; }
+  public string Country { get; private set; } = "BR";
+  public string? ZipCode { get; private set; }
+  public User? User { get; private set; }
+  public Tenant? Tenant { get; private set; }
 }
