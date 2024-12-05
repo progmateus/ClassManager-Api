97ddedea69469b91001127ea6a7a89b0bf6bff95
Mateus Lopes <devmateustools@gmail.com>
2024-12-05 16:16:04 -0300

    feat: entity address

---
+++ src/ClassManager.Domain/Contexts/Address/Entities/Address.cs
@@ -0,0 +1,28 @@
+using ClassManager.Domain.Contexts.Accounts.Entities;
+using ClassManager.Domain.Contexts.Tenants.Entities;
+using ClassManager.Domain.Shared.Entities;
+
+namespace ClassManager.Domain.Contexts.Address.Entites
+{
+  public class Address : Entity
+  {
+    protected Address() { }
+    public Address(string street, string city, string state)
+    {
+      Street = street;
+      City = city;
+      State = state;
+    }
+    public Guid? UserId { get; private set; }
+    public Guid? TenantId { get; private set; }
+    public string Street { get; private set; }
+    public string? Number { get; private set; }
+    public string? Neighborhood { get; private set; }
+    public string City { get; private set; }
+    public string State { get; private set; }
+    public string Country { get; private set; } = "BR";
+    public string? ZipCode { get; private set; }
+    public User? User { get; private set; }
+    public Tenant? Tenant { get; private set; }
+  }
+}
