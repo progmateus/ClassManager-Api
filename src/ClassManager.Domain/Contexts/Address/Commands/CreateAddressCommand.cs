d2b837a3114a925a79a0fa811f14a9096081aff4
Mateus Lopes <devmateustools@gmail.com>
2024-12-05 18:21:43 -0300

    feat: crate address command

---
+++ src/ClassManager.Domain/Contexts/Address/Commands/CreateAddressCommand.cs
@@ -0,0 +1,23 @@
+using ClassManager.Domain.Shared.Commands;
+using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace ClassManager.Domain.Contexts.Tenants.Commands
+{
+  public class CreateAddressCommand : Notifiable, ICommand
+  {
+    public string Street { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public string State { get; set; } = null!;
+    public string Number { get; set; } = null!;
+
+    public void Validate()
+    {
+      AddNotifications(new Contract()
+      .Requires()
+      .IsNotNullOrEmpty(Street, "Street", "Street cannot be null")
+      .IsNotNullOrEmpty(City, "City", "City cannot be null")
+    );
+    }
+  }
+}
