454d1e13e1f206c9f09244cb8ba46d72a0086d3a
Mateus Lopes <devmateustools@gmail.com>
2024-12-06 01:43:44 -0300

    feat: delete address handler

---
+++ src/ClassManager.Domain/Contexts/Address/Handlers/DeleteAddressHandler.cs
@@ -0,0 +1,69 @@
+using AutoMapper;
+using ClassManager.Domain.Contexts.Accounts.Repositories.Contracts;
+using ClassManager.Domain.Contexts.Addresses.Entites;
+using ClassManager.Domain.Contexts.Addresses.Repositories.Contracts;
+using ClassManager.Domain.Contexts.Plans.Repositories;
+using ClassManager.Domain.Contexts.Roles.Entities;
+using ClassManager.Domain.Contexts.Roles.Repositories.Contracts;
+using ClassManager.Domain.Contexts.Roles.ViewModels;
+using ClassManager.Domain.Contexts.Shared.Enums;
+using ClassManager.Domain.Contexts.Shared.ValueObjects;
+using ClassManager.Domain.Contexts.Subscriptions.Entities;
+using ClassManager.Domain.Contexts.tenants.ViewModels;
+using ClassManager.Domain.Contexts.Tenants.Commands;
+using ClassManager.Domain.Contexts.Tenants.Entities;
+using ClassManager.Domain.Contexts.Tenants.Repositories.Contracts;
+using ClassManager.Domain.Services.Stripe.Repositories.Contracts;
+using ClassManager.Domain.Shared.Commands;
+using ClassManager.Domain.Shared.Services.AccessControlService;
+using ClassManager.Shared.Commands;
+using ClassManager.Shared.Handlers;
+using Flunt.Notifications;
+
+namespace ClassManager.Domain.Contexts.Tenants.Handlers;
+
+public class DeleteAddressHandler : Notifiable
+{
+  private readonly ITenantRepository _tenantRepository;
+  private readonly IMapper _mapper;
+  private readonly IAccessControlService _accessControlService;
+  private readonly IAddressRepository _addressRepository;
+
+  public DeleteAddressHandler(
+    ITenantRepository tenantRepository,
+    IMapper mapper,
+    IAccessControlService accessControlService,
+    IAddressRepository addressRepository
+
+    )
+  {
+    _tenantRepository = tenantRepository;
+    _mapper = mapper;
+    _accessControlService = accessControlService;
+    _addressRepository = addressRepository;
+  }
+  public async Task<ICommandResult> Handle(Guid loggedUserId, Guid addressId)
+  {
+    var address = await _addressRepository.GetByIdAsync(addressId, new CancellationToken());
+
+    if (address is null)
+    {
+      return new CommandResult(false, "ERR_ADDRESS_NOT_FOUND", null, null, 404);
+    }
+
+    if (address.UserId.HasValue && address.UserId != Guid.Empty && !loggedUserId.Equals(address.UserId.Value))
+    {
+      return new CommandResult(false, "ERR_PERMISSION_DENIED", null, null, 403);
+    }
+
+
+    if (address.TenantId.HasValue && address.TenantId != Guid.Empty && !await _accessControlService.HasUserAnyRoleAsync(loggedUserId, address.TenantId.Value, ["admin"]))
+    {
+      return new CommandResult(false, "ERR_PERMISSION_DENIED", null, null, 403);
+    }
+
+    await _addressRepository.DeleteAsync(address.Id, new CancellationToken());
+
+    return new CommandResult(true, "ADDRESS_DELETED", new { }, null, 201);
+  }
+}
