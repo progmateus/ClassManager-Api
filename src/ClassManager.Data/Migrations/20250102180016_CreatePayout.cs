33023b9b92ecdbdc59209f625d25bebd2d219f6c
Mateus Lopes <devmateustools@gmail.com>
2025-01-02 15:00:39 -0300

    feat: payout migration

---
+++ src/ClassManager.Data/Migrations/20250102180016_CreatePayout.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClassManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatePayout : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payouts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    VARCHAR = table.Column<string>(type: "varchar(100)", maxLength: 200, nullable: false),
+                    Amount = table.Column<decimal>(type: "DECIMAL(18,0)", nullable: false),
+                    Currency = table.Column<string>(type: "VARCHAR(20)", maxLength: 20, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TenantId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payouts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payouts_Tenants_TenantId",
+                        column: x => x.TenantId,
+                        principalTable: "Tenants",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payouts_TenantId",
+                table: "Payouts",
+                column: "TenantId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payouts");
+        }
+    }
+}
