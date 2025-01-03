338fe085cc8987fa026dd89c9523cded705ba524
Mateus Lopes <devmateustools@gmail.com>
2025-01-03 15:28:40 -0300

    feat: add plan id to subscription migration

---
+++ src/ClassManager.Data/Migrations/20250103182522_AddPlanIdToSubscription.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClassManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlanIdToSubscription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "TenantPlanId",
+                table: "Subscriptions",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "PlanId",
+                table: "Subscriptions",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "TargetType",
+                table: "Subscriptions",
+                type: "TINYINT",
+                nullable: false,
+                defaultValue: (byte)1);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscriptions_PlanId",
+                table: "Subscriptions",
+                column: "PlanId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subscriptions_Plans_PlanId",
+                table: "Subscriptions",
+                column: "PlanId",
+                principalTable: "Plans",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subscriptions_Plans_PlanId",
+                table: "Subscriptions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Subscriptions_PlanId",
+                table: "Subscriptions");
+
+            migrationBuilder.DropColumn(
+                name: "PlanId",
+                table: "Subscriptions");
+
+            migrationBuilder.DropColumn(
+                name: "TargetType",
+                table: "Subscriptions");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "TenantPlanId",
+                table: "Subscriptions",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
