a4fa8e85c2f8229cf9c8e9ef90d508527cd13479
Mateus Lopes <devmateustools@gmail.com>
2025-01-08 19:34:54 -0300

    feat: external bank account and payout status enums

--- src/ClassManager.Domain/Contexts/Shared/Enums/EPayoutStatus.cs
+++ src/ClassManager.Domain/Contexts/Shared/Enums/EPayoutStatus.cs
@@ -2,10 +2,8 @@ namespace ClassManager.Domain.Contexts.Shared.Enums
 {
   public enum EPayoutStatus
   {
-    PAID = 1,
-    PENDING = 2,
-    IN_TRANSIT = 3,
-    CANCELED = 4,
-    FAILED = 5
+    PENDING = 1,
+    CANCELED = 2,
+    CONCLUDED = 3
   }
 }
