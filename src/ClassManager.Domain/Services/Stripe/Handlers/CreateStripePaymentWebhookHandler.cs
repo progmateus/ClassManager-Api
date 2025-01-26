a33f8cd29582e8780cc2d3d643d8978057a193c7
Mateus Lopes <devmateustools@gmail.com>
2025-01-26 00:40:34 -0300

    feat: creating payment amount in decimal

--- src/ClassManager.Domain/Services/Stripe/Handlers/CreateStripePaymentWebhookHandler.cs
+++ src/ClassManager.Domain/Services/Stripe/Handlers/CreateStripePaymentWebhookHandler.cs
@@ -31,7 +31,9 @@ public class CreateStripePaymentWebhookHandler
       return;
     }
 
-    var payment = new Contexts.Invoices.Entities.Payment(stripeInvoice.Total, stripeInvoice.Currency, invoiceEntity.TenantId, invoiceEntity.Id, invoiceEntity.TargetType);
+    var amount = stripeInvoice.Total / 100;
+
+    var payment = new Contexts.Invoices.Entities.Payment(amount, stripeInvoice.Currency, invoiceEntity.TenantId, invoiceEntity.Id, invoiceEntity.TargetType);
 
     await _paymentRepository.CreateAsync(payment, new CancellationToken());
   }
